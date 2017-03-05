c1e9e24411ad00a9b947e65b40cd1d47ae99dc05
wt911122 <wt911122@163.com>
2017-03-05 17:21:52 +0800

    add changes to app.js

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-
 App({
-  onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
+    onLaunch: function() {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+    },
+    getUserInfo: function(cb) {
+        var that = this
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function() {
+                    wx.getUserInfo({
+                        success: function(res) {
+                            that.globalData.userInfo = res.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
         }
-      })
+    },
+    globalData: {
+        userInfo: null
+    },
+    cloneObject: function(src) {
+        var target = {};
+        for (var entry in src) {
+            if (src.hasOwnProperty(entry)) {
+                target[entry] = src[entry];
+            }
+        }
+        return target;
     }
-  },
-  globalData:{
-    userInfo:null
-  }
 })
-
-
